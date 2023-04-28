2f70e04243fc90ea86a93b4bac957010cb889802
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-04-27 21:33:27 -0700

    Find the area of the rectangle
    Da chay nhung van con mot so loi

---
+++ AreaR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AreaR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float dai, rong;
+
+            Console.Write("Nhap Chieu Dai: ");
+            dai = float.Parse(s: Console.ReadLine());
+            Console.Write("Nhap Chieu Rong: ");
+            rong = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Dien tich chieu hinh chu nha:"+ (dai*rong));
+        }
+    }
+}
